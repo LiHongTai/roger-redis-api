83afe99adf6478a56ed3e6b81374bc93fcf19719
LiHongTai <631396529@qq.com>
2018-12-18 10:33:34 +0800

    通过反射获取当前类的属性或者其父类的属性值

--- src/main/java/com/roger/redis/utils/ReflectUtil.java
+++ src/main/java/com/roger/redis/utils/ReflectUtil.java
@@ -4,17 +4,20 @@ import java.lang.reflect.Field;
 
 public class ReflectUtil {
 
-    public static Field getField(Class<?> clazz,String fieldName){
+    public static Field getField(Class<?> clazz, String fieldName) {
         try {
             return clazz.getDeclaredField(fieldName);
         } catch (NoSuchFieldException e) {
-           return null;
+            if(!clazz.getSuperclass().isAssignableFrom(Object.class)){
+                return getField(clazz.getSuperclass(),fieldName);
+            }
+            return null;
         }
     }
 
-    public static <T> T getObject(Object obj,String fieldName){
+    public static <T> T getObject(Object obj, String fieldName) {
         try {
-            Field field = getField(obj.getClass(),fieldName);
+            Field field = getField(obj.getClass(), fieldName);
             field.setAccessible(true);
             return (T) field.get(obj);
         } catch (IllegalAccessException e) {
